dbff16b0f201fa463da8b75770ba6738b488f637
Edoardo Costantini <costantini.edoardo@yahoo.com>
2019-02-07 14:38:45 +0100

    Reorganize Folder

--- testSuite/PDFimport-testSuite.R
+++ testSuite/PDFimport-testSuite.R
@@ -5,3 +5,4 @@ test_file("PDFimport-test.R")
 # 16 more extractions than in reference
 # -1 # of errors: statcheck misses 1 error
 # -1 # of decision errors: statcheck misses 1 error
+auto_test("/Users/Edoardo/DriveUni/gh-statcheck/R","/Users/Edoardo/DriveUni/gh-statcheck/test")
